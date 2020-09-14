84d0b1589a4cd3ebb2f2d94d25170f062be7b571
Bizwayo Kasaro <bizwayokasaro2@hotmail.com>
2020-09-14 17:39:14 -0500

    file reading and example test text file added

--- main.java
+++ main.java
@@ -1,3 +1,24 @@
-public static void main(String[] args) {
-       
+import java.io.*;
+
+class main{
+
+    public static void main(String[] args) {
+        String[] ciphertext = new String[100000];
+        try{
+            BufferedReader inFile = new BufferedReader(new FileReader("test.txt"));
+            String line="";
+            int i=0;
+
+            while((line = inFile.readLine()) != null) {
+                String[] words = line.trim().split("");
+                for(int j=0;j<words.length;j++){
+                    ciphertext[i++] = words[j];
+                }
+            }
+            inFile.close();
+        }
+        catch(IOException e){
+            System.out.println("ERROR: "+ e.getMessage());
+        }
+    }
 }
